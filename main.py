cc5883002a16643ed51a6e4b8ed3e916ac9a2214
hunterbukhari <hunterbukhari@gmail.com>
2025-05-12 00:15:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from routers import auth, attendance, export_excel
+from database import init_db
 
 app = FastAPI()
+
+init_db()
 
 app.include_router(auth.router, prefix="/auth")
 app.include_router(attendance.router, prefix="/attendance")
